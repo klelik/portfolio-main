e94dde11ff4d2739ef29c30ae93a6faabb5f0242
klelik <klelika1998@gmail.com>
2022-12-22 20:43:15 +0000

    added scroll magic -> about

--- src/about/about.js
+++ src/about/about.js
@@ -71,3 +71,20 @@ window.addEventListener('scroll',function() {
 //     popupMenu.classList.remove('popup-menu-open');
 //   });
 // });
+
+//Scroll Magic
+var controller = new ScrollMagic.Controller();
+
+// build scene
+new ScrollMagic.Scene({
+          triggerElement: "#trigger1",
+          triggerHook: 0.9, // show, when scrolled 10% into view
+          duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+          offset: 50 // move trigger to center of element
+        })
+        .setClassToggle("#reveal1", "visible") // add class to reveal
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+console.log(controller);
