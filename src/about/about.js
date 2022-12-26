78b9179bd1de3b0ff4dc52380988c8ae0c91d356
klelik <klelika1998@gmail.com>
2022-12-26 03:48:29 +0000

    almost finished about page

--- src/about/about.js
+++ src/about/about.js
@@ -1,20 +1,42 @@
-var options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-var prnDt =  new Date().toLocaleTimeString('en-us', options);
 
-const dateElement = document.getElementById("date")
-const redCircle = document.getElementsByClassName("circle")
-const yellowCircle = document.getElementsByClassName("circle-yellow")
-const red = document.getElementById("red")
-const yellow = document.getElementById("yellow")
-dateElement.textContent ="-"+ prnDt.slice(0,21)+"th-";
+var options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false,
+};
+var prnDt = new Date().toLocaleTimeString("en-us", options);
+
+const dateElement = document.getElementById("date");
+const redCircle = document.getElementsByClassName("circle");
+const yellowCircle = document.getElementsByClassName("circle-yellow");
+const red = document.getElementById("red");
+const nameBig = document.getElementById("name");
+const yellow = document.getElementById("yellow");
+dateElement.textContent = "-" + prnDt.slice(0, 21) + "th-";
+
+let posRed = red.offsetTop;
+let posYellow = yellow.offsetTop;
+let posName = nameBig.offsetTop;
+console.log(posRed);
+console.log(posYellow);
+console.log(posName);
+
 
 //parallax
 
-window.addEventListener('scroll',function() {
-    let value = window.scrollY;
-    red.style.top = 80 + value * 0.30 + 'px';
-    yellow.style.top = 470 + value * 0.65 + 'px';
-})
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  red.style.top = posRed + value * 0.3 + "px";
+  nameBig.style.top = posName + value * -0.1 + "px";
+  yellow.style.top = posYellow + value * 0.65 + "px";
+});
+
 
 // window.addEventListener("scroll", function() {
 //     const distance = window.scrollY;
@@ -23,7 +45,6 @@ window.addEventListener('scroll',function() {
 //     document.querySelector(
 //       ".parallax"
 //     ).style.transform = `translateY(${distance * 0.1}px)`;
-
 
 //     $(document).ready(function(){
 //         $(".menu-icon").on("click", function(){
@@ -39,10 +60,6 @@ window.addEventListener('scroll',function() {
 //             $('nav').removeClass('nav-bg');
 //         }
 //     });
-
-
-
-
 
 // const navbar = document.querySelector('.navbar');
 // const burgerMenu = document.querySelector('.burger-menu');
@@ -76,15 +93,83 @@ window.addEventListener('scroll',function() {
 var controller = new ScrollMagic.Controller();
 
 // build scene
-new ScrollMagic.Scene({
-          triggerElement: "#trigger1",
-          triggerHook: 0.9, // show, when scrolled 10% into view
-          duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-          offset: 50 // move trigger to center of element
-        })
-        .setClassToggle("#reveal1", "visible") // add class to reveal
-        .addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+var firstFadeIn = new ScrollMagic.Scene({
+							triggerElement: "#trigger1",
+							triggerHook: 0.9,
+							offset: 100, // move trigger to center of element
+							reverse: false // only do once
+						})
+						.setClassToggle("#reveal1", "visible") // add class toggle
+						// .addIndicators() // add indicators (requires plugin)
+						.addTo(controller);
 
+// var secondFadeIn = new ScrollMagic.Scene({
+//     triggerElement: "#trigger2",
+//     triggerHook: 0.9,
+//     offset: 100, // move trigger to center of element
+//     reverse: false // only do once
+// })
+// .setClassToggle("#reveal1", "visible") // add class toggle
+// .addIndicators() // add indicators (requires plugin)
+// .addTo(controller);
+
+// build scene contact form
 
 console.log(controller);
+
+//go top
+// var btn = $('#button');
+
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:3}, '1600');
+// });
+
+//scroll to top, fade in when scroll is 100 px before the end of the page
+$(document).ready(function(){
+	$(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+
+        if (scroll > $(document).height() - $(window).height() - 350) {
+            // $('#back-to-top').fadeIn();
+            $('.next').stop().fadeTo(500,1);
+
+        } else { //when scroll is <window-height - 100 disappear
+            // $('#back-to-top').fadeOut();
+
+            // $('.next').stop().fadeTo(500,0);
+
+        }
+
+        if ($(this).scrollTop() > $(document).height() - $(window).height() - 100) {
+				$('#back-to-top').fadeIn();
+                // $('.next').fadeIn();
+
+			} else { //when scroll is <window-height - 100 disappear
+				$('#back-to-top').fadeOut();
+
+                // $('.next').fadeOut();
+
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 200);
+			return false;
+		});
+});
+
+var Scrollbar = window.Scrollbar;
+
+Scrollbar.init(document.querySelector('body'),options);
